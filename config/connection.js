e36890e3f0f9b1f072821eaeaa85b04925538727
Brookee <oliver.brooke.a@gmail.com>
2019-07-30 13:58:31 -0400

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,30 @@
+// Dependencies
 var mysql = require("mysql");
+// creating mySQL connection 
+var connected; 
+
+// Heroku connection 
+if(process.env.JAWSDB_URL){
+connected = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connected.mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "user",
+        password: "",
+        database: "burgers_db"
+    })
+};
+
+// mySQL connection
+connected.connect(function(error){
+    if (error) {
+        console.log("mySQL connection error " + err.stack);
+        return;
+    }
+    console.log("Connected to mySQL database with id: " + connected.threadId);
+});
+
+// Export connection
+module.exports = connected;
+
