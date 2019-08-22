fb3fef53828565a66eac316417862f069427f61f
Brookee <oliver.brooke.a@gmail.com>
2019-08-22 00:32:58 -0400

    port added

--- config/orm.js
+++ config/orm.js
@@ -68,30 +68,5 @@ var orm = {
     });
   }
 };
-// var orm = {
-//   selectAll: function(table, cb) {
-//     var queryStr = 'SELECT * FROM ' + table + ";" ;
-//     console.log(queryStr);
-//     connected.query(queryStr, function(error, res) {
-//       if (error) throw error;
-//       cb(res);
-//     });
-//   },
-//   insertOne: function(table, col, newInput, callback){
-//     var queryStr = 'INSERT INTO ' + table + "(" + col + ") VALUES (" + newInput + ");";
-//     connected.query(queryStr, function(error, data) {
-//         if (error) throw error;
-//         callback(data);
-//     })
-//   },
-//   updateOne: function(table, col, updateInput, callback){
-//     var queryStr = "UPDATE " + table + 'SET ' + col + '=?' + 'WHERE ' +  updateInput;
-//     connected.query(queryStr, [table, col, updateInput], function(error, data){
-//         if (error) throw error;
-//         callback(data);
-//     })
-//   }
-// }
-
 // Export ORM
 module.exports = orm;
